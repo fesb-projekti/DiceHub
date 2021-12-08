1dd8d5ab1b2d0c4c7c22bf387007ebf9226661f3
Kristian Džidžić <wowisnikr@gmail.com>
2021-12-08 22:56:02 +0100

    MainLayout implemented
    - The basic look of the site has been implemented. Here we are rendering the header(navbar), the sidebar and the components in use (main - passed as props from parent component depending on the path/route)
    - Basic toggle state for the sidebar

--- client/src/layout/MainLayout.jsx
+++ client/src/layout/MainLayout.jsx
@@ -1,14 +1,21 @@
 import classes from "./MainLayout.module.css";
 import MainNavbar from "./MainNavbar";
 import MainSidebar from "./MainSidebar";
+import {useState} from "react";
 
 function MainNavigation(props) {
     // This is the main layout for the application, we are getting props from app and router and then we are displaying
     // that page with props.children. <MainNavbar/> is always on top as a navbar for the whole application, rest is changing.
+    const [isCollapsed,setIsCollapsed] = useState(true);
+
+    function toggleSidebar(){
+        setIsCollapsed(!isCollapsed);
+    }
+
     return (
         <div className={classes.mainLayout}>
-            <MainNavbar />
-            <MainSidebar /> 
+            <MainNavbar sidebar={toggleSidebar} />
+            <MainSidebar active={isCollapsed} />
             <main className={classes.main}>{props.children}</main>
         </div>
     );
