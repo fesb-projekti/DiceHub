cceaafd076fcd5dcaa2267d06dce014b1fbd1ce8
Kristian Džidžić <wowisnikr@gmail.com>
2021-12-08 22:53:50 +0100

    MainSidebar & Style
    - Added the sidebar with basic links and style
    - Animation on toggle. The animation depends on the state from parent, with the state we choose different class to achieve this

--- client/src/layout/MainSidebar.jsx
+++ client/src/layout/MainSidebar.jsx
@@ -1,11 +1,29 @@
 import classes from "./MainSidebar.module.css";
+import { Link } from "react-router-dom";
 
-function MainSidebar() {
-    return ( 
-        <div className={classes.sidebar}>
-            <h1>Sidebar</h1>
-        </div>
-     );
+function MainSidebar({ active }) {
+    // Using two returns with same items, but different classes to achieve a css transition
+    if (active)
+        return (
+            <div className={classes.sidebar}>
+                <span><Link to="/chat">Chat</Link></span>
+                <span><Link to="/profile">Profile</Link></span>
+                <span><Link to="/inventory">Inventroy</Link></span>
+                <span><Link to="/settings">Settings</Link></span>
+                <span><Link to="/about">About</Link></span>
+                <span>Logout</span>
+            </div >
+        )
+    else return (
+        <div className={classes.hidden}>
+            <span><Link to="/chat">Chat</Link></span>
+            <span><Link to="/profile">Profile</Link></span>
+            <span><Link to="/inventory">Inventroy</Link></span>
+            <span><Link to="/settings">Settings</Link></span>
+            <span><Link to="/about">About</Link></span>
+            <span>Logout</span>
+        </div >
+    )
 }
 
 export default MainSidebar;
