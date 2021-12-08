0459f379faf6a9b1f225d7b25fae6e6e40514628
Kristian Džidžić <wowisnikr@gmail.com>
2021-12-08 23:00:13 +0100

    Mainnavbar implemented & Style + fd4fdf5 Hotfix
    - Implemented the navigation bar
    - Using a callback for the sidebar toggle
    - Rough style for the navbar
    
    - Skipped one predefined page style, adding it now

--- client/src/layout/MainNavbar.jsx
+++ client/src/layout/MainNavbar.jsx
@@ -1,14 +1,15 @@
 import classes from "./MainNavbar.module.css";
 import { Link } from "react-router-dom";
 
-function MainNavbar() {
+function MainNavbar(props) {
+
     return (
         <header className={classes.header}>
             <nav>
                 <ul>
-                    <li><Link to="/chat">Chat</Link></li>
+                    <li><Link to="/chat">FunctionIcon</Link></li>
                     <li className={classes.logo}><Link to="/">DiceHub</Link></li>
-                    <li>SideBarIcon</li>
+                    <li className={classes.toggle} onClick={()=>props.sidebar()}>SideBarIcon</li>
                 </ul>
             </nav>
         </header >
