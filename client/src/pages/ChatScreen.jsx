f0dce404cf2dfa4e8db5e433a1184a03796667fb
Trpimir <t.rajevskij@yahoo.com>
2021-12-14 11:44:32 +0100

    Chat module done

--- client/src/pages/ChatScreen.jsx
+++ client/src/pages/ChatScreen.jsx
@@ -6,11 +6,19 @@ import "./ChatScreen.css";
 
 function ChatScreen() {
 
+    const[input, setInput] = useState("");
     const [messages, setMessages] = useState([
         {
 
         }
     ])
+
+    const handleSend = e =>{
+        e.preventDefault();
+        
+        setMessages([...messages, {message: input}]);
+        setInput('');
+    };
 
   return (
         <div className="chatScreen">
@@ -23,15 +31,26 @@ function ChatScreen() {
                             alt={message.name}
                             src={message.image}
                         />
-                        <p className="ChatScreen_text">{message}</p>
+                        <p className="ChatScreen_text">{message.message}</p>
                     </div>
                 ) : (
                     <div className="chatScreen__message">
-                        <p className="ChatScreen_textUser">{message}</p>
+                        <p className="ChatScreen_textUser">{message,message}</p>
                     </div>
                 )
             ))}
-        </div>
+            <div>
+                <form  className="chatScreen__input">
+                    <input 
+                    value={input}
+                    onChange={e=> setInput(e.target.value)}
+                    className='chatScreen__inputField'
+                    placeholder='Write a message' type = "text"/>
+                    <button onClick={handleSend}
+                    type="submit" className='chatScreen__inputButton'>SEND</button>
+                </form>
+            </div>
+                    </div>
 
     );
                 }
