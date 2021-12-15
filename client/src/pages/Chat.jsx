bc9cf63db746e02059523db34318405b04edbaf9
Trpimir <t.rajevskij@yahoo.com>
2021-12-15 16:24:53 +0100

    Chat module
    Chat module

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,32 +1,58 @@
+import React, { useState } from 'react';
 import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
-import ChannelContainer from '../components/ChannelContainer';
-import ChannelListContainer from '../components/ChannelListContainer';
+
+import { ChannelListContainer, ChannelContainer, Auth } from '../components/Chat';
+
+import 'stream-chat-react/dist/css/index.css';
 import './Chat.css';
 
+const cookies = new Cookies();
 
 const apiKey = '9savhsb8uzh5';
+const authToken = cookies.get("token");
 
 const client = StreamChat.getInstance(apiKey);
 
-const _Chat = () => {
-    return(
-        
+if(authToken) {
+    client.connectUser({
+        id: cookies.get('userId'),
+        name: cookies.get('username'),
+        fullName: cookies.get('fullName'),
+        image: cookies.get('avatarURL'),
+        hashedPassword: cookies.get('hashedPassword'),
+        phoneNumber: cookies.get('phoneNumber'),
+    }, authToken)
+}
+
+
+const App = () => {
+    const [createType, setCreateType] = useState('');
+    const [isCreating, setIsCreating] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
+
+    if(!authToken) return <Auth />
+
+    return (
         <div className="app__wrapper">
-            
-            <Chat client = {client} theme="team light">
+            <Chat client={client} theme="team light">
                 <ChannelListContainer 
-                
+                    isCreating={isCreating}
+                    setIsCreating={setIsCreating}
+                    setCreateType={setCreateType}
+                    setIsEditing={setIsEditing}
                 />
-
-                <ChannelContainer
-
+                <ChannelContainer 
+                    isCreating={isCreating}
+                    setIsCreating={setIsCreating}
+                    isEditing={isEditing}
+                    setIsEditing={setIsEditing}
+                    createType={createType}
                 />
             </Chat>
         </div>
     );
 }
 
-
-export default _Chat;
+export default Chat;
