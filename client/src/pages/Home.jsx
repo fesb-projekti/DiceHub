bf7d0b6117d3297979cdbff530b7874f534f7e90
Kristian Džidžić <wowisnikr@gmail.com>
2021-12-20 01:31:49 +0100

    Advanced card controls
    - Added functionality to the buttons for like/dislike and undo
    Cards can be now restored if there was a mistake in the choice, also you can swipe by using the like/dislike buttons

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,16 +1,25 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef, useMemo, createRef } from 'react'
 import SwipeCard from "../components/SwipeCard";
 import classes from "./Home.module.css";
 
 function Home() {
 
   const [profileCards, setProfileCards] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState();
   const [lastDirection, setLastDirection] = useState();
+
+  const currentIndexRef = useRef(currentIndex);
+
+  const childRefs = useMemo(() =>
+  Array(profileCards.length)
+    .fill(0)
+    .map((i) => createRef()), []);
 
   useEffect(() => {
     const getProfileCards = async () => {
       const profileCardsFromServer = await fetchProfileCards();
       setProfileCards(profileCardsFromServer);
+      setCurrentIndex(profileCardsFromServer.length)
     }
     getProfileCards();
   }, [])
@@ -21,20 +30,47 @@ function Home() {
     return data;
   }
 
-  const swiped = (direction, nameToDelete) => {
-    console.log('removing: ' + nameToDelete);
-    setLastDirection(direction);
+  const updateCurrentIndex = (val) => {
+    setCurrentIndex(val);
+    currentIndexRef.current = val;
   }
 
-  const outOfFrame = (name) => {
-    console.log(name + ' left the screen!')
+  const canGoBack = currentIndex < profileCards.length;
+  const canSwipe = currentIndex >= 0;
+
+  const swiped = (direction, nameToDelete, index) => {
+    setLastDirection(direction);
+    updateCurrentIndex(index - 1);
+  }
+
+  const swipe = async (dir) => {
+    if (canSwipe && currentIndex < profileCards.length) {
+      await childRefs[currentIndex].current.swipe(dir);
+    }
+  }
+
+  const outOfFrame = (name, idx) => {
+    console.log(`${name} (${idx}) left the screen!`, currentIndexRef.current);
+    currentIndexRef.current >= idx && childRefs[idx].current.restoreCard();
+  }
+
+  const goBack = async () => {
+    if (!canGoBack) return
+    const newIndex = currentIndex + 1
+    updateCurrentIndex(newIndex)
+    await childRefs[newIndex].current.restoreCard()
   }
 
   return (
     <div className={classes.home}>
       <div className={classes.cardContainer}>
-        {profileCards.map((character) =>
-          <SwipeCard className={classes.swipe} key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
+        {profileCards.map((character, index) =>
+          <SwipeCard className={classes.swipe}
+            ref={childRefs[index]}
+            key={character.name}
+            onSwipe={(dir) => swiped(dir, character.name, index)}
+            onCardLeftScreen={() => outOfFrame(character.name, index)}
+          >
             <div className={classes.card} style={{ backgroundImage: 'url(' + character.url + ')' }}>
               <h3>{character.name}</h3>
             </div>
@@ -65,11 +101,11 @@ function Home() {
       </div>
       {lastDirection ? <h3 className={classes.infoText}>You swiped {lastDirection}</h3> : <h4 className={classes.infoText} />}
       <div className={classes.buttons}>
-        <i><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none" /><path d="M15 3H6c-.83 0-1.54.5-1.84 1.22l-3.02 7.05c-.09.23-.14.47-.14.73v2c0 1.1.9 2 2 2h6.31l-.95 4.57-.03.32c0 .41.17.79.44 1.06L9.83 23l6.59-6.59c.36-.36.58-.86.58-1.41V5c0-1.1-.9-2-2-2zm4 0v12h4V3h-4z" /></svg></i>
-        <i><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none" /><path d="M12.5 8c-2.65 0-5.05.99-6.9 2.6L2 7v9h9l-3.62-3.62c1.39-1.16 3.16-1.88 5.12-1.88 3.54 0 6.55 2.31 7.6 5.5l2.37-.78C21.08 11.03 17.15 8 12.5 8z" /></svg></i>
-        <i><img src="https://img.icons8.com/metro/26/000000/info.png"/></i>
-        <i><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none" /><path d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z" /></svg></i>
-        <i><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none" /><path d="M1 21h4V9H1v12zm22-11c0-1.1-.9-2-2-2h-6.31l.95-4.57.03-.32c0-.41-.17-.79-.44-1.06L14.17 1 7.59 7.59C7.22 7.95 7 8.45 7 9v10c0 1.1.9 2 2 2h9c.83 0 1.54-.5 1.84-1.22l3.02-7.05c.09-.23.14-.47.14-.73v-2z" /></svg></i>
+        <button onClick={() => swipe("left")}><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none" /><path d="M15 3H6c-.83 0-1.54.5-1.84 1.22l-3.02 7.05c-.09.23-.14.47-.14.73v2c0 1.1.9 2 2 2h6.31l-.95 4.57-.03.32c0 .41.17.79.44 1.06L9.83 23l6.59-6.59c.36-.36.58-.86.58-1.41V5c0-1.1-.9-2-2-2zm4 0v12h4V3h-4z" /></svg></button>
+        <button onClick={() => goBack()}><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none" /><path d="M12.5 8c-2.65 0-5.05.99-6.9 2.6L2 7v9h9l-3.62-3.62c1.39-1.16 3.16-1.88 5.12-1.88 3.54 0 6.55 2.31 7.6 5.5l2.37-.78C21.08 11.03 17.15 8 12.5 8z" /></svg></button>
+        <button><img src="https://img.icons8.com/metro/26/000000/info.png" /></button>
+        <button><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none" /><path d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z" /></svg></button>
+        <button onClick={() => swipe("right")}><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none" /><path d="M1 21h4V9H1v12zm22-11c0-1.1-.9-2-2-2h-6.31l.95-4.57.03-.32c0-.41-.17-.79-.44-1.06L14.17 1 7.59 7.59C7.22 7.95 7 8.45 7 9v10c0 1.1.9 2 2 2h9c.83 0 1.54-.5 1.84-1.22l3.02-7.05c.09-.23.14-.47.14-.73v-2z" /></svg></button>
       </div>
     </div>
   )
