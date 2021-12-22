877e2559bddf39e3ab2b6d31417b1c4c0425d693
Kristian Džidžić <kristian334@gmail.com>
2021-12-22 20:24:05 +0100

    Default sidebar state
    - Sidebar is now hidden by default, untill open

--- client/src/layout/MainLayout.jsx
+++ client/src/layout/MainLayout.jsx
@@ -6,7 +6,7 @@ import {useState} from "react";
 function MainNavigation(props) {
     // This is the main layout for the application, we are getting props from app and router and then we are displaying
     // that page with props.children. <MainNavbar/> is always on top as a navbar for the whole application, rest is changing.
-    const [isCollapsed,setIsCollapsed] = useState(true);
+    const [isCollapsed,setIsCollapsed] = useState(false);
 
     function toggleSidebar(){
         setIsCollapsed(!isCollapsed);
