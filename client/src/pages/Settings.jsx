a7abcd94fb40d6950c09759b34f8a6d3b0a9121d
Kristian Džidžić <wowisnikr@gmail.com>
2021-12-22 13:47:15 +0100

    Settings update
    - Fixed the required field
    - Empty fields will be ignored in backend, only non empty values will be updated

--- client/src/pages/Settings.jsx
+++ client/src/pages/Settings.jsx
@@ -16,8 +16,6 @@ function Settings() {
     const onSubmit = async (event) => {
         event.preventDefault();
         const name = nameInputRef.current.value;
-        //if(name == "")
-            // Pass default event
         const surname = surnameInputRef.current.value;
         const password = passwordInputRef.current.value;
         const repeatPassword = repeatPasswordInputRef.current.value;
@@ -30,10 +28,9 @@ function Settings() {
             alert("Passwords are not maching!");
             return;
         }
-
         const profile = { name, surname, password, repeatPassword, age, location, about, hasLocation };
         await fetch("http://localhost:3001/update_profile", {
-            method: "POST",
+            method: "PUT",
             headers: { "Content-type": "application/json" },
             body: JSON.stringify(profile)
         }).then(() => { alert("You have updated your profile information"); history("/profile/:id") })
@@ -45,35 +42,35 @@ function Settings() {
             <form className={classes.form} onSubmit={onSubmit}>
                 <div className={classes.control}>
                     <label htmlFor="name">Name</label>
-                    <input type="text" required id="name" placeholder="Example: John" ref={nameInputRef} />
+                    <input type="text" id="name" placeholder="Example: John" ref={nameInputRef} />
                 </div>
                 <div className={classes.control}>
                     <label htmlFor="surname">Surname</label>
-                    <input type="text" required id="surname" placeholder="Example: Ford" ref={surnameInputRef} />
+                    <input type="text" id="surname" placeholder="Example: Ford" ref={surnameInputRef} />
                 </div>
                 <div className={classes.control}>
                     <label htmlFor="password">Password</label>
-                    <input type="text" required id="password" placeholder="Example: 12345678" ref={passwordInputRef} />
+                    <input type="text" id="password" placeholder="Example: 12345678" ref={passwordInputRef} />
                 </div>
                 <div className={classes.control}>
                     <label htmlFor="repeat_password">Repeat password</label>
-                    <input type="text" required id="repeat_password" placeholder="Example: 12345678" ref={repeatPasswordInputRef} />
+                    <input type="text" id="repeat_password" placeholder="Example: 12345678" ref={repeatPasswordInputRef} />
                 </div>
                 <div className={classes.control}>
                     <label htmlFor="age">Age</label>
-                    <input type="text" required id="age" placeholder="Enter your age" ref={ageInputRef} />
+                    <input type="text" id="age" placeholder="Enter your age" ref={ageInputRef} />
                 </div>
                 <div className={classes.control}>
                     <label htmlFor="location">Location</label>
-                    <input type="text" required id="location" placeholder="What is your location?" ref={locationInputRef}></input>
+                    <input type="text" id="location" placeholder="What is your location?" ref={locationInputRef}></input>
                 </div>
                 <div className={classes.control}>
                     <label htmlFor="about">About myself</label>
-                    <textarea id="about" required rows="3" placeholder="Update information about yourself" ref={aboutInputRef}></textarea>
+                    <textarea id="about" rows="3" placeholder="Update information about yourself" ref={aboutInputRef}></textarea>
                 </div>
                 <div className={classes.control}>
                     <label htmlFor="has_location">Location to play</label>
-                    <input type="text" required id="has_location" placeholder="Do I have a location to play?" ref={hasLocationInputRef}></input>
+                    <input type="text" id="has_location" placeholder="Do I have a location to play?" ref={hasLocationInputRef}></input>
                 </div>
                 <div className={classes.actions}>
                     <button>Update settings</button>
