b71b4421421cb9466cb6b40e645b471b4ebccb45
Kristian Džidžić <kristian334@gmail.com>
2022-01-16 20:03:07 +0100

    Basic login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,14 +1,44 @@
 import classes from "./Login.module.css";
+import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
-    return ( 
+    
+    const history = useNavigate();
+    const nameInputRef = useRef();
+    const passwordInputRef = useRef();
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const name = nameInputRef.current.value;
+        const password = passwordInputRef.current.value;
+
+        const info = { name,password };
+        await fetch("http://localhost:3001/authorize", {
+            method: "PUT",
+            headers: { "Content-type": "application/json" },
+            body: JSON.stringify(info)
+        }).then(() => { history("/") })
+    }
+
+    return (
         <div className={classes.login}>
-            <h2>Login is rendered here</h2>
-            <h3>Content</h3>
-            <h3>Content2</h3>
-            <h3>Content3</h3>
+            <div className={classes.loginBox}>
+                <h2>DiceHub login form</h2>
+                <form className={classes.form} onSubmit={onSubmit}>
+                    <div className={classes.control}>
+                        <label htmlFor="username">Username</label>
+                        <input type="text" id="username" placeholder="Example: John31" ref={nameInputRef} />
+                    </div>
+                    <div className={classes.control}>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" placeholder="Example: 12345678" ref={passwordInputRef} />
+                    </div>
+                <button>Login</button>
+                </form>
+            </div>
         </div>
-     );
+    );
 }
 
 export default Login;
