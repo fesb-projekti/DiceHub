2be62c14b1669d13202a541613e852c8199f00e6
dVlah99 <vlah.danijel37@gmail.com>
2022-01-17 18:53:36 +0100

    removed redundant code

--- server/server.js
+++ server/server.js
@@ -36,7 +36,6 @@ app.get("/profileCards/allProfile", (req, res) => {
     const ID = req.body.ID
     const sqlSelect = "SELECT ime, prezime, username, hasLocation, COUNT(inventar.igraID) AS brojigara, kategorija.naziv AS FavKat FROM korisnik LEFT JOIN inventar ON korisnik.ID = inventar.korisnikID LEFT JOIN igre ON inventar.igraID = igre.ID LEFT JOIN kategorija ON korisnik.favGenre = kategorija.ID WHERE korisnik.favGenre = ALL (SELECT DISTINCT kategorija.ID FROM kategorija WHERE kategorija.ID = korisnik.favGenre) GROUP BY korisnik.ID";
     db.query(sqlSelect, [ID], (err, result) => {
-        console.log(result)
         res.send(result)
     })
 });
