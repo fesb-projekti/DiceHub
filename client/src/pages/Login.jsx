101f380ff451450ba48d41096183e8248b3ba82a
Kristian Džidžić <kristian334@gmail.com>
2022-01-18 23:03:17 +0100

    Login valid information check

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,26 +2,33 @@ import classes from "./Login.module.css";
 import { useRef } from "react";
 import { useNavigate } from "react-router-dom";
 
-function Login({login}) {
-    
+function Login({ login }) {
+
     const history = useNavigate();
     const nameInputRef = useRef();
     const passwordInputRef = useRef();
 
     const onSubmit = async (event) => {
         event.preventDefault();
-        const name = nameInputRef.current.value;
-        const password = passwordInputRef.current.value;
+        const username = nameInputRef.current.value;
+        const passw = passwordInputRef.current.value;
+        const info = { username, passw };
 
-        const info = { name,password };
-        await fetch("http://localhost:3001/login", {
+        const result = await fetch("https://dice-hub.ga/api/login", {
             method: "POST",
             headers: { "Content-type": "application/json" },
             body: JSON.stringify(info)
-        }).then(() => { history("/") })
+        });
+        const data = await result.json();
+        if (data[0]?.id != undefined) {
+            history("/");
+            localStorage.setItem("username", data[0]?.username);
+            login(true);
+        }
+        else{
+            alert("Incorrect login information!");
+        }
 
-        localStorage.setItem("username", name);
-        login(true);
     }
 
     return (
@@ -37,7 +44,7 @@ function Login({login}) {
                         <label htmlFor="password">Password</label>
                         <input type="password" id="password" placeholder="Example: 12345678" ref={passwordInputRef} />
                     </div>
-                <button>Login</button>
+                    <button>Login</button>
                 </form>
             </div>
         </div>
